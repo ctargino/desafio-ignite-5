e26fa7976fdfae1bbc59ba3eed132714cbd57c6b
Caio Targino <c.targino@gmail.com>
2021-03-28 20:25:01 -0300

    Initial commit

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+
+import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+class ListAllUsersController {
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const { user_id } = request.headers;
+
+    try {
+      const users = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+
+      return response.json(users);
+    } catch (err) {
+      return response
+        .status(400)
+        .json({ error: "User is not a admin or does not exist" });
+    }
+  }
+}
+
+export { ListAllUsersController };
